9f78930db0006a77492032e72f4b200f9c7c4455
Khizer-khan701 <khizerdawood987@gmail.com>
2025-09-24 18:26:05 +0500

    changes done

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from qa_app import views
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', views.ask_question, name='home'),
     path("", include("qa_app.urls")),  # hook qa_app urls
 ]
